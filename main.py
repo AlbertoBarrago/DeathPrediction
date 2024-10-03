fbdba71e0f08ce43e7ebd9571d9e71ddcb84b47c
Alberto Barrago <albertobarrago@gmail.com>
2024-10-03 12:50:18 +0200

    Added favicon.ico

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 from pydantic import BaseModel
 from fastapi import FastAPI, Request, Form, HTTPException
@@ -5,7 +7,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from dataclasses import dataclass
 import markdown
-
+from starlette.responses import FileResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -13,6 +15,13 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get('/favicon.ico')
+async def favicon():
+    file_name = "favicon.ico"
+    file_path = os.path.join(app.root_path, "static", file_name)
+    return FileResponse(path=file_path, headers={"Content-Disposition": "attachment; filename=" + file_name})
 
 @dataclass
 class GenerateRequest(BaseModel):
