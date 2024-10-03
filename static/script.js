c770e71f6cbbf20585341123abe0c3116d1abcca
Alberto Barrago <albertobarrago@gmail.com>
2024-10-03 18:17:00 +0200

    Added Some File for deploy

--- static/script.js
+++ static/script.js
@@ -23,20 +23,22 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     DOWNLOAD_BTN.addEventListener('click', function () {
-        if(RESULT.innerText.trim() === "") {
+        if (RESULT.innerText.trim() === "") {
             alert('Compile form before')
             return;
         }
 
-        const {jsPDF} = window.jspdf;
-
-        const doc = new jsPDF();
-
         const content = document.getElementById('result').textContent || 'No result to display';
 
-        doc.text(content, 10, 10);
+        const blob = new Blob([content], {type: 'text/plain'});
 
-        doc.save('prediction.pdf');
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'prediction.txt';
+
+        link.click();
+
+        URL.revokeObjectURL(link.href);
     });
 
     CLEAR_BTN.addEventListener('click', function () {
